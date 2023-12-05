688ade1b04fa3e2e77b73ef8f8802e0d99b4d495
Mai Hoàng Phương Anh <90672443+Phanhxinh@users.noreply.github.com>
2023-12-05 23:57:32 +0700

    add thuoc tinh

---
+++ src/main/java/com/project/DuAnTotNghiep/repository/BrandRepository.java
@@ -0,0 +1,8 @@
+package com.project.DuAnTotNghiep.repository;
+
+import com.project.DuAnTotNghiep.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    boolean existsByCode(String code);
+}
